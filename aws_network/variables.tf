2e390be3fd0013f7713f9082e38c5675c07be5a4
Sergei Chesnokov <ches.tranveks@gmail.com>
2024-07-17 21:06:15 -0400

    Module to create AWS VPC Network

---
+++ aws_network/variables.tf
@@ -0,0 +1,30 @@
+
+variable "region" {
+  description = "Please Enter AWS Region to deploy Server:"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "public_subnet_cidrs" {
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  default = [
+    "10.0.11.0/24",
+    "10.0.22.0/24",
+    "10.0.33.0/24"
+  ]
+}
